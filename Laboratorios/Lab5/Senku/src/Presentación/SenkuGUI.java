a4e37b9b029eadf57299ba213275bfb2faa98b8f
RiCHi <60302534+Ricardo-Olarte@users.noreply.github.com>
2021-11-05 09:47:24 -0500

    Add files via upload

--- Laboratorios/Lab5/Senku/src/Presentación/SenkuGUI.java
+++ Laboratorios/Lab5/Senku/src/Presentación/SenkuGUI.java
@@ -2,25 +2,83 @@ package Presentación;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class SenkuGUI extends JFrame {
 
     private static final Dimension DIMENSION = Toolkit.getDefaultToolkit().getScreenSize();
     private static final int ALTO = DIMENSION.height;
     private static final int ANCHO = DIMENSION.width;
+    private JMenuBar menuBar;
+    private JMenu archivo;
+    private JMenuItem nuevo,abrir,salvar,salir;
 
     /**
-     *
+     * Constructor de la clase SenkuGUI
      */
-    public void prepareElemntos(){
-        this.setTitle("Senku");
-        this.setSize( ANCHO/2,ALTO/2);
+    public SenkuGUI(){
+        prepareElementos();
+        prepareAcciones();
     }
 
     /**
-     *
+     * Prepara los elementos de la ventana
+     */
+    public void prepareElementos(){
+        this.setTitle("Senku");
+        this.setSize( ANCHO/2,ALTO/2);
+        setLocationRelativeTo(null);
+        prepareElementosMenu();
+    }
+
+    /**
+     * Prepara los componentes del menu
+     */
+    public void prepareElementosMenu(){
+        menuBar = new JMenuBar();
+
+        archivo = new JMenu("Archivo");
+
+        nuevo = new JMenuItem("Nuevo");
+        abrir = new JMenuItem("Abrir");
+        salvar = new JMenuItem("Salvar");
+        salir = new JMenuItem("Salir");
+
+        archivo.add(nuevo);
+        archivo.add(abrir);
+        archivo.add(salvar);
+        archivo.add(salir);
+
+        menuBar.add(archivo);
+        setJMenuBar(menuBar);
+    }
+
+    /**
+     * Prepara las acciones
      */
     public void prepareAcciones(){
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                accionCerrar();
+            }
+        });
+    }
 
+    /**
+     * Cierra la ventana
+     */
+    public void accionCerrar(){
+        int r = JOptionPane.showConfirmDialog(null,"¿Está seguro que desea salir?");
+        if(r==JOptionPane.YES_OPTION){
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        SenkuGUI ventana = new SenkuGUI();
+        ventana.setVisible(true);
     }
 }
